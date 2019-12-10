3e1255a358d74f3e36b0874c2202eb55a4a2f73f
Yiming <yiming@luminate.one>
2019-12-10 17:07:58 +1300

    added foreign  key

--- src/Models/SingleModelRevision.php
+++ src/Models/SingleModelRevision.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace LuminateOne\RevisionTracking\Models;
 
 use Illuminate\Database\Eloquent\Model;
@@ -7,21 +8,24 @@ use Illuminate\Support\Facades\Schema;
 
 class SingleModelRevision extends Model
 {
-   protected $fillable = ['revision_identifiers', 'original_values'];
+    protected $fillable = ['revision_identifiers', 'original_values', 'revisions_version_id'];
 
 
     /**
      *  Create a revision table for this Model
      */
-    public function createTableIfNotExists(){
+    public function createTableIfNotExists()
+    {
+        if (Schema::hasTable($this->table)) return;
 
-       if(Schema::hasTable($this->table)) return;
+        Schema::create($this->table, function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('revisions_version_id');
+            $table->text('revision_identifiers');
+            $table->text('original_values');
+            $table->timestamps();
 
-       Schema::create($this->table, function (Blueprint $table) {
-           $table->bigIncrements('id');
-           $table->string('revision_identifiers');
-           $table->string('original_values');
-           $table->timestamps();
-       });
-   }
+            $table->foreign('revisions_version_id')->references('id')->on('revisions_versions');
+        });
+    }
 }
