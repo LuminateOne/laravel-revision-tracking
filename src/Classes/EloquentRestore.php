af596e737bab6f0f1f8f92c809739f1a124828a3
Yiming <yiming@luminate.one>
2019-12-11 14:18:29 +1300

    Added a way to restore a revision for a specific Model

--- src/Classes/EloquentRestore.php
+++ src/Classes/EloquentRestore.php
@@ -10,31 +10,50 @@ use LuminateOne\RevisionTracking\Models\RevisionsVersion;
 
 class EloquentRestore
 {
+    /**
+     * @param null $targetModelName
+     * @throws ErrorException
+     */
+    public static function handle($targetModelName = null)
+    {
+        if(!$targetModelName){
+            $latestRevisionVersion = RevisionsVersion::latest('id')->first();
+
+            if (!$latestRevisionVersion) {
+                Log::info("No revisions found");
+                return;
+            }
+
+            $targetModelName = $latestRevisionVersion->model_name;
+        }
+
+        if (!class_exists($targetModelName)) {
+            throw new ErrorException('The target Model: ' . $targetModelName . ' does not exist, looks like you changed the model name.');
+        }
+
+        self::restore($targetModelName);
+    }
 
     /**
      *  Restoring the revision.
      *  Get the latest record from revisions_versions table.
      *  Get the latest record from the revision table.
+     * @param $targetModelName
+     * @throws ErrorException
      */
-    public static function restore()
+    public static function restore($targetModelName)
     {
-        $latestRevisionVersion = RevisionsVersion::latest('id')->first();
+        $latestRevisionVersion = RevisionsVersion::where(['model_name' => $targetModelName])->latest('id')->first();
 
-        if(!$latestRevisionVersion){
-            Log::info("No revisions found");
+        if (!$latestRevisionVersion) {
+            Log::info("No revisions found for Model: ". $targetModelName);
             return;
-        }
-
-        $targetModelName = $latestRevisionVersion->model_name;
-
-        if(!class_exists($targetModelName)){
-            throw new ErrorException('The target Model: ' . $targetModelName . ' does not exist, looks like you changed the model name.');
         }
 
         $targetModel = new $targetModelName();
 
         $revisionTableName = config('revision_tracking.table_prefix', 'revisions_') . $targetModel->getTable();
-        if(!Schema::hasTable($revisionTableName)){
+        if (!Schema::hasTable($revisionTableName)) {
             throw new ErrorException("No revisions found for " . $targetModelName . ', looks like you changed the model name or revision table name.');
         }
 
