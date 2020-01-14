a56c0b310e29e6c4d7ed2a0fb237ad889db80aff
Yiming <yiming@luminate.one>
2020-01-15 09:02:14 +1300

    Refactored the eloquentStoreDiff function
    Refactored the eloquentStoreDiff function

--- src/RevisionTracking.php
+++ src/RevisionTracking.php
@@ -55,7 +55,7 @@ class RevisionTracking
         }
 
         $revisionModel->revision_identifier = $model->revisionIdentifier();
-        $revisionModel->original_values = serialize($originalFields);
+        $revisionModel->original_values = $originalFields;
 
         $revisionModel->save();
     }
