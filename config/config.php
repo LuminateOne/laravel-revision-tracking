459ec0d97f0715432654320b8674e3a9a8acd47f
Yiming <yiming@luminate.one>
2020-01-17 14:31:15 +1300

    Refactored the description of config file
    Refactored the description of config file

--- config/config.php
+++ config/config.php
@@ -14,6 +14,7 @@ return [
     |--------------------------------------------------------------------------
     |
     */
+
     'table_prefix' => env('REVISION_TABLE_PREFIX', 'revisions_'),
 
     /*
@@ -21,8 +22,8 @@ return [
     | Defines the action of revisionable after delete a model
     |--------------------------------------------------------------------------
     |
-    | If set to true the revisions will be deleted, after the model is deleted,
-    | If set to false the revision will not be deleted, after the model is deleted
+    | true => Revisions will be deleted, after the model is deleted,
+    | false => Revision will not be deleted, after the model is deleted
     */
 
     'remove_on_delete' => env('REVISION_REMOVE_ON_DELETE', false),
